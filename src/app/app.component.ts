5eb7f87b86a9bfd55c7dc12b4057a9187580b4ed
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-21 20:04:11 +0300

    Changed application's name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
-  message='Welcome to my todo-app'
+  title = 'Smart Todo App';
+  message='Welcome to MuresanIA';
 }
