db67a02d13486b2ffc27829a795422bdf5e848d3
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-23 23:55:42 +0300

    Created a Todo Class

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,12 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id : 1, description: 'Develop Logic Skills'},
-    {id : 2, description: 'Develop Angular Skills'},
-    {id : 3, description: 'Develop Rest Api'}
+    new Todo(1, 'Develop Logic Skills', false, new Date()),
+    new Todo(2, 'Develop Angular Skills', false, new Date()),
+    new Todo(3, 'Develop Rest Api', false, new Date())
+    // { id: 1, description: '' },
+    // { id: 2, description: '' },
+    // { id: 3, description: '' }
   ]
 
 
