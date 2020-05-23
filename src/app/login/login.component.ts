b0015f1d997104e92222899f755158b9b5d53d60
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-23 19:58:23 +0300

    Implementation Login #4 added ngModel to username and password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username='alex'
+  password=''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    console.log(this.username);
   }
 
 }
