d91c9c66840844186122bd2c2911c2ca0b014c03
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-28 21:03:16 +0300

    Connecting Angular Frontend with Restful API - Creating Data Service

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor() { }
+  executeHelloWorldBeanService(){
+    console.log("Execute Hello World Bean Service")
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 
 @Component({
@@ -13,17 +14,21 @@ message = 'Some Welcome Message'
 name=''
 
 
-//ActivatedRoute
-  constructor(private route: ActivatedRoute) { 
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { 
 
   }
 
   
   ngOnInit(): void {
-    //COMPILATION ERROR this.message = 5
-    console.log(this.message)
-    // console.log(this.route.snapshot.params['name'])
+   
     this.name = this.route.snapshot.params['name'];
   }
+
+getWelcomeMessage(){
+  this.service.executeHelloWorldBeanService();
+}
 
 }
