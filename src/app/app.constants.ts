0773f3e60c0638dcfc5e25aa453b480e01c7bfec
MuresanIA <muresan.alexandru933@gmail.com>
2020-07-08 22:41:34 +0300

    connected to db

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
 export const API_URL ="http://localhost:8080"
 
-export const TODO_JPA_API_URL ="http://localhost:8080/jpa"
+export const TODO_JPA_API_URL ="https://todo-app7-backend.herokuapp.com/"
